26e8e817f919ec6a39633767b8853b3334f4aaeb
Cuza <baopham92@yahoo.com>
2019-08-21 10:46:12 -0400

    created event emmitter, createProduct() & written tests

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.5.0;
 
+// Beginning of a smart contract
 contract Marketplace {
+    // Publicly available / accessible features of a smart contract
     string public name;
     uint public productCount = 0;
     mapping(uint => Product) public products;
@@ -13,15 +15,43 @@ contract Marketplace {
         bool purchased;
     }
 
+    // event to log console logs
+    event ProductCreated (
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
+
+    // Function that runs one time for smart contract
     constructor() public {
         name = "KK Marketplace";
     }
 
     function createProduct(string memory _name, uint _price) public {
         // Verify parameters
+        // Require _name
+        require(bytes(_name).length > 0);
+        // Require _price
+        require(_price > 0);
         // Incremenet product count
         productCount ++; 
         // Create product
+        products[productCount] = Product(
+            productCount,
+            _name,
+            _price,
+            msg.sender,
+            false
+            );
         // Trigger event
+        emit ProductCreated(
+            productCount,
+            _name,
+            _price,
+            msg.sender,
+            false
+        )
     }
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,6 +1,15 @@
 const Marketplace = artifacts.require('./Marketplace.sol')
 
-contract('Marketplace', (accounts) => {
+// Requre "chai" package & uses
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+contract('Marketplace', ([
+  deployer,
+  seller,
+  buyer
+]) => {
   let marketplace
 
   before(async () => {
@@ -27,12 +36,22 @@ contract('Marketplace', (accounts) => {
     let result, productCount
 
     before(async () => {
-      result = await marketplace.createProduct('KK Random Item', web3.utils.toWei('1', 'Ether'))
-      productCount = await marketplace.productCount()
+      result = await marketplace.createProduct('KK Random Item', web3.utils.toWei('1', 'Ether'), { from: seller });
+      productCount = await marketplace.productCount();
     })
 
     it('creates a product', async () => {
+      const event = result.logs[0].args;
+
+      // Successful tests
       assert.equal(productCount, 1);
+      assert.equal(event.id.toNumber(), productCount.toNumber() , 'Succeeded');
+      assert.equal(event.name, 'KK Random Item', 'Succeeded');
+      assert.equal(event.price, '1000000000000000000', 'Succeeded');
+      assert.equal(event.owner, seller, 'Succeeded');
+      assert.equal(event.purchased, false , 'Succeeded');
+
+      // Failed tests
     })
   })
 })
